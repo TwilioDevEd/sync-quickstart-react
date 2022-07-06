40b5430cf00e4287f8ad81f87cd484a7cc35a658
David Prothero <dprothero@twilio.com>
2022-07-06 15:18:42 -0700

    Refactored SyncedInputField to function component

--- src/app/SyncedInputField.js
+++ src/app/SyncedInputField.js
@@ -1,34 +1,24 @@
 import React from "react";
 
-class SyncedInputField extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFocused: false,
-    };
-  }
-
-  render() {
-    return (
-      <input
-        onFocus={(e) => {
-          this.setState({ isFocused: true });
-        }}
-        onBlur={(e) => {
-          this.setState({ isFocused: false });
-          this.props.setFormValue(this.props.formDataKey, e.target.value);
-        }}
-        id={this.props.formDataKey}
-        type="text"
-        onChange={(e) => {
-          this.props.setFormValue(this.props.formDataKey, e.target.value);
-        }}
-        value={this.props.formDataValue}
-        className="form-control cb-input"
-        placeholder={this.props.placeholder}
-      />
-    );
-  }
+export default function SyncedInputField({
+  setFormValue,
+  formDataKey,
+  formDataValue,
+  placeholder,
+}) {
+  return (
+    <input
+      onBlur={(e) => {
+        setFormValue(formDataKey, e.target.value);
+      }}
+      id={formDataKey}
+      type="text"
+      onChange={(e) => {
+        setFormValue(formDataKey, e.target.value);
+      }}
+      value={formDataValue}
+      className="form-control cb-input"
+      placeholder={placeholder}
+    />
+  );
 }
-
-export default SyncedInputField;
