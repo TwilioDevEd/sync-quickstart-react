74c8ce6b8c1f26d80f388415c25bf2b8aa024d8b
David Prothero <dprothero@twilio.com>
2022-07-06 14:36:06 -0700

    Fix failing unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Twilio Sync/i);
+  const linkElement = screen.getByText(
+    /Twilio Sync Quickstart with React and Node.js/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
