88f80f040e733d4086ebed9f4f681134aabc8748
David Prothero <dprothero@twilio.com>
2022-07-07 12:24:32 -0700

    Reintegrated remaining functions into Functional component

--- src/app/SyncCobrowsing.js
+++ src/app/SyncCobrowsing.js
@@ -6,88 +6,6 @@ import { SyncClient } from "twilio-sync";
 import Participants from "./Participants.js";
 import SyncedInputField from "./SyncedInputField";
 
-// Participant routines
-function addParticipant(client, identity, sessionId, setParticipants) {
-  let map;
-  const participantsMapKey = "participants-" + sessionId;
-
-  client.map(participantsMapKey).then((participantMap) => {
-    map = participantMap;
-
-    function refreshParticipants() {
-      getAllItems(map).then((items) => {
-        const newParticipants = items.map((item) => item.data);
-        console.log("participants", newParticipants);
-        setParticipants(newParticipants);
-      });
-    }
-
-    map
-      .addListener("itemAdded", refreshParticipants)
-      .addListener("itemUpdated", refreshParticipants)
-      .addListener("itemRemoved", refreshParticipants)
-      .set(identity, {
-        identity: identity,
-      })
-      .then((item) => {
-        console.log("Added: ", item.key);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  });
-
-  // Return a cleanup function
-  return () => {
-    map
-      .removeAllListeners()
-      .remove(identity)
-      .then(() => {
-        console.log("Participant " + identity + " removed.");
-      })
-      .catch((error) => {
-        console.error("Error removing: " + identity, error);
-      });
-  };
-}
-
-// Since Sync Map has pagination we need to navigate through all the pages
-async function getAllItems(map) {
-  const result = [];
-  let page = await map.getItems();
-  result.push(...page.items);
-
-  while (page.hasNextPage) {
-    page = await page.nextPage();
-    result.push(...page.items);
-  }
-  return result;
-}
-
-// Document routines
-function updateSyncDocument(client, sessionId, formData) {
-  if (!client) {
-    return;
-  }
-  client.document(sessionId).then((doc) => {
-    doc.set(formData);
-  });
-}
-
-function loadFormData(client, sessionId, setFormData) {
-  client.document(sessionId).then((doc) => {
-    setFormData(doc.data);
-
-    doc.on("updated", (event) => {
-      console.log("Sync Updated Data", event);
-      if (!event.isLocal) {
-        console.log("Setting state with", event.data);
-        setFormData(event.data);
-      }
-    });
-  });
-}
-
 // React component
 export default function SyncCobrowsing({ identity, sessionId }) {
   const [status, setStatus] = useState("Connecting...");
@@ -122,6 +40,76 @@ export default function SyncCobrowsing({ identity, sessionId }) {
       }
     }
 
+    function loadFormData() {
+      clientRef.current.document(sessionId).then((doc) => {
+        setFormData(doc.data);
+
+        doc.on("updated", (event) => {
+          console.log("Sync Updated Data", event);
+          if (!event.isLocal) {
+            console.log("Setting state with", event.data);
+            setFormData(event.data);
+          }
+        });
+      });
+    }
+
+    function addParticipant() {
+      let map;
+      const participantsMapKey = "participants-" + sessionId;
+
+      clientRef.current.map(participantsMapKey).then((participantMap) => {
+        map = participantMap;
+
+        // Since Sync Map has pagination we need to navigate through all the pages
+        async function getAllItems(map) {
+          const result = [];
+          let page = await map.getItems();
+          result.push(...page.items);
+
+          while (page.hasNextPage) {
+            page = await page.nextPage();
+            result.push(...page.items);
+          }
+          return result;
+        }
+
+        function refreshParticipants() {
+          getAllItems(map).then((items) => {
+            const newParticipants = items.map((item) => item.data);
+            console.log("participants", newParticipants);
+            setParticipants(newParticipants);
+          });
+        }
+
+        map
+          .addListener("itemAdded", refreshParticipants)
+          .addListener("itemUpdated", refreshParticipants)
+          .addListener("itemRemoved", refreshParticipants)
+          .set(identity, {
+            identity: identity,
+          })
+          .then((item) => {
+            console.log("Added: ", item.key);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      });
+
+      cleanupRef.current = () => {
+        map
+          .removeAllListeners()
+          .remove(identity)
+          .then(() => {
+            console.log("Participant " + identity + " removed.");
+          })
+          .catch((error) => {
+            console.error("Error removing: " + identity, error);
+          });
+      };
+    }
+
     function createSyncClient(token) {
       const client = new SyncClient(token, { logLevel: "info" });
 
@@ -129,14 +117,8 @@ export default function SyncCobrowsing({ identity, sessionId }) {
         if (state === "connected") {
           clientRef.current = client;
           setStatus("connected");
-          loadFormData(client, sessionId, setFormData);
-          const cleanup = addParticipant(
-            client,
-            identity,
-            sessionId,
-            setParticipants
-          );
-          cleanupRef.current = cleanup;
+          loadFormData();
+          addParticipant();
         } else {
           setStatus("error");
           setErrorMessage(`Error: expected connected status but got ${state}`);
@@ -157,9 +139,18 @@ export default function SyncCobrowsing({ identity, sessionId }) {
     };
   }, [identity, sessionId]);
 
+  function updateSyncDocument(newFormData) {
+    if (!clientRef.current) {
+      return;
+    }
+    clientRef.current.document(sessionId).then((doc) => {
+      doc.set(newFormData);
+    });
+  }
+
   function setFormValue(fieldName, value) {
     const newFormData = { ...formData, [fieldName]: value };
-    updateSyncDocument(clientRef.current, sessionId, newFormData);
+    updateSyncDocument(newFormData);
     setFormData(newFormData);
   }
 
