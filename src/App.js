149595499c3604c0c3bd286d95de46b8659aff2c
David Prothero <dprothero@twilio.com>
2022-07-07 12:55:00 -0700

    Convert App to Functional component

--- src/App.js
+++ src/App.js
@@ -1,111 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import SyncCobrowsing from "./app/SyncCobrowsing.js";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+export default function App() {
+  // Check for identity in query string
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
 
-    // Check for identity in query string
-    const params = new Proxy(new URLSearchParams(window.location.search), {
-      get: (searchParams, prop) => searchParams.get(prop),
-    });
+  const [sessionId, setSessionId] = useState("session123");
+  const [identity, setIdentity] = useState(params?.identity || "");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    this.state = {
-      sessionId: "session123",
-      identity: params?.identity || "",
-      isLoggedIn: false,
-    };
-  }
-
-  setSessionId(event) {
-    this.setState({ sessionId: event.target.value });
-  }
-
-  setIdentity(event) {
-    this.setState({ identity: event.target.value });
-  }
-
-  login() {
-    this.setState({ isLoggedIn: true });
-  }
-
-  logout() {
-    this.setState({ isLoggedIn: false });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Twilio Sync Quickstart with React and Node.js</h1>
-          <p>
-            Learn more about{" "}
-            <a href="https://www.twilio.com/sync">Twilio Sync</a>
-          </p>
-          <div className="logout-container">
-            {this.state.isLoggedIn && (
-              <button
-                className="btn btn-primary"
-                onClick={(e) => this.logout()}
-              >
-                Logout
-              </button>
-            )}
-          </div>
-        </header>
-        {this.state.identity.length > 0 && this.state.isLoggedIn ? (
-          <SyncCobrowsing
-            sessionId={this.state.sessionId}
-            identity={this.state.identity}
-          />
-        ) : (
-          <div className="container">
-            <div className="card border-primary">
-              <div className="card-body text-info">
-                <h5 className="card-title">Session Id</h5>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  Provide a cobrowsing session id - should be unique
-                </h6>
-                <div className="input-group mb-3">
-                  <input
-                    type="text"
-                    placeholder="Session Id"
-                    defaultValue={this.state.sessionId}
-                    onChange={(e) => this.setSessionId(e)}
-                  ></input>
-                </div>
-              </div>
-            </div>
-            <div className="card border-primary">
-              <div className="card-body text-info">
-                <h5 className="card-title">Identity</h5>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  Provide a unique identity for each user joining the cobrowsing
-                  session
-                </h6>
-                <div className="input-group mb-3">
-                  <input
-                    type="text"
-                    placeholder="Identity (username)"
-                    defaultValue={this.state.identity}
-                    onChange={(e) => this.setIdentity(e)}
-                  ></input>
-                  <button
-                    className="btn btn-primary"
-                    onClick={(e) => this.login()}
-                  >
-                    Login
-                  </button>
-                </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Twilio Sync Quickstart with React and Node.js</h1>
+        <p>
+          Learn more about <a href="https://www.twilio.com/sync">Twilio Sync</a>
+        </p>
+        <div className="logout-container">
+          {isLoggedIn && (
+            <button
+              className="btn btn-primary"
+              onClick={() => setIsLoggedIn(false)}
+            >
+              Logout
+            </button>
+          )}
+        </div>
+      </header>
+      {identity.length > 0 && isLoggedIn ? (
+        <SyncCobrowsing sessionId={sessionId} identity={identity} />
+      ) : (
+        <div className="container">
+          <div className="card border-primary">
+            <div className="card-body text-info">
+              <h5 className="card-title">Session Id</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                Provide a cobrowsing session id - should be unique
+              </h6>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  placeholder="Session Id"
+                  defaultValue={sessionId}
+                  onChange={(e) => setSessionId(e.target.value)}
+                ></input>
               </div>
             </div>
           </div>
-        )}
-      </div>
-    );
-  }
+          <div className="card border-primary">
+            <div className="card-body text-info">
+              <h5 className="card-title">Identity</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                Provide a unique identity for each user joining the cobrowsing
+                session
+              </h6>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  placeholder="Identity (username)"
+                  defaultValue={identity}
+                  onChange={(e) => setIdentity(e.target.value)}
+                ></input>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => setIsLoggedIn(true)}
+                >
+                  Login
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default App;
